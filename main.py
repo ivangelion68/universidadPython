f858bf4cb53d628761a7eb975d9e0b5f419f1056
Ivan Altamirano (Tbay) <ivan.altamirano@outlook.com>
2022-04-21 22:21:31 -0400

    tarea 1 de seccion 3

--- main.py
+++ main.py
@@ -12,15 +12,17 @@
 
 # Entrada de Usuario
 # Funcion Input para procesar la entrada del usuario
-numero1 = input("Ingrese el primer número:")
-numero2 = input("Ingrese el segundo numero:")
-resultado = int(numero2) + int(numero1);
-print("Escribió:",resultado,"del tipo",type(resultado),"en la seccion de memoria",id(resultado))
-print("Fin del Programa")
+# numero1 = input("Ingrese el primer número:")
+# numero2 = input("Ingrese el segundo numero:")
+# resultado = int(numero2) + int(numero1);
+# print("Escribió:",resultado,"del tipo",type(resultado),"en la seccion de memoria",id(resultado))
+# print("Fin del Programa")
+#
+# if type(resultado) == str:
+#     print("Resultado es string")
+# else:
+#     print("Resultado no es string")
 
-if type(resultado) == str:
-    print("Resultado es string")
-else:
-    print("Resultado no es string")
 
-
+calificacion = input("Como estuvo tu dia?:")
+print("Tu dia estuvo del",calificacion)
