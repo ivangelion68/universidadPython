dd31401702b542a969091fd17210f756125ecca1
Ivan Altamirano (Tbay) <ivan.altamirano@outlook.com>
2022-04-21 22:09:47 -0400

    Breve If / Else... repaso de los booleans

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-# # x = 10
-# # y = 2
-# # z = x + y
-# # print(z)
-# # cadena:str = "Hola Mundo"
-# # dos:bool=True
-# # ## Indica la posición de memoria de la variable
-# # print(type(cadena))
-# # print(type(dos))
-#
-# # Boolean
-# x = False
+# Boleanos
+miVariable = False
+print(miVariable)
 
-# Cadena (String)
-grupoFavorito = 'Los Bunkers'
-grupoFavorito2 = 'Dua Lipa'
-print("Mi grupo favorito es " + grupoFavorito)
-print("Mi cantante favorito es:", grupoFavorito2)
+miVariable = 3 > 2
+print(miVariable)
 
-numero1 = "1"
-numero2 = "2"
+if  miVariable:
+    print("el resultado fue verdadero")
+else:
+    print("El resultado es falso")
 
-suma = int(numero1) + int(numero2)
-print(suma)
