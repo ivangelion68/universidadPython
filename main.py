01e1fa37b09599d95b55ae2f8b91cb1440c7bade
Ivan Altamirano (Tbay) <ivan.altamirano@outlook.com>
2022-04-21 22:14:05 -0400

    Ingreso de valores por consola

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-# Boleanos
-miVariable = False
-print(miVariable)
+# # Boleanos
+# miVariable = False
+# print(miVariable)
+#
+# miVariable = 3 > 2
+# print(miVariable)
+#
+# if  miVariable:
+#     print("el resultado fue verdadero")
+# else:
+#     print("El resultado es falso")
 
-miVariable = 3 > 2
-print(miVariable)
-
-if  miVariable:
-    print("el resultado fue verdadero")
-else:
-    print("El resultado es falso")
-
+# Entrada de Usuario
+# Funcion Input para procesar la entrada del usuario
+print("Ingrese Valor:")
+resultado = input();
+print("Escribió:",resultado,"del tipo",type(resultado),"en la seccion de memoria",id(resultado))
+print("Fin del Programa")
